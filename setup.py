f2218b3e7f8756bcce1b0863cd9f1e445fdf2bbb
zhangjx <1075495028@qq.com>
2020-06-24 14:52:50 +0800

    pyharbortools to pyharbortool

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyharbortools",
+    name="pyharbortool",
     version="0.1",
     author="zhangjx",
     author_email="1075495029@qq.com",
